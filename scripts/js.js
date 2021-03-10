ca34cf51c76e7c24d5504db13a8ed7cf662d47b4
Gabriel Sykes <48891219+sykesgabri@users.noreply.github.com>
2021-03-10 15:33:42 +0000

    Add files via upload

--- scripts/js.js
+++ scripts/js.js
@@ -14,3 +14,15 @@ function topButt(){
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+function mobileAlert(x) {
+  if (x.matches) { // If media query matches
+    alert("You appear to be on mobile, we have a mobile optimized site at 'm.contrastcrew.co.uk'.");
+  } else {
+   null;
+  }
+}
+
+var x = window.matchMedia("(max-width: 700px)")
+mobileAlert(x) // Call listener function at run time
+x.addListener(mobileAlert) // Attach listener function on state changes
